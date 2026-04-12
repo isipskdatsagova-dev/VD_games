53188456d6caa7d14abdf8eb09bb9c46e831eeda
isipskdatsagova-dev <isip_s.k.datsagova@mpt.ru>
2026-04-12 17:50:38 +0300

    Update gcd.py

--- VD_games/games/gcd.py
+++ VD_games/games/gcd.py
@@ -7,7 +7,6 @@ RULES = "Find the greatest common divisor of given numbers."
 
 
 def generate_round():
-    """Генерирует два числа и правильный ответ (НОД)."""
     num1 = random.randint(1, 100)
     num2 = random.randint(1, 100)
     question = f"{num1} {num2}"
