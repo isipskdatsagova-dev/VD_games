2a5ff5f4109ff6b4ad010279dfeaceee0164b785
isipskdatsagova-dev <isip_s.k.datsagova@mpt.ru>
2026-04-12 17:52:48 +0300

    Update engine.py

--- VD_games/engine.py
+++ VD_games/engine.py
@@ -4,7 +4,6 @@ import prompt
 
 
 def run_game(game_module):
-    """Запускает игру с переданной логикой."""
     print("Welcome to the VD Games!")
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
