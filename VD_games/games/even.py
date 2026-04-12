c867c00462c120a7955000e1946d00fe53691832
isipskdatsagova-dev <isip_s.k.datsagova@mpt.ru>
2026-04-12 17:50:24 +0300

    Update even.py

--- VD_games/games/even.py
+++ VD_games/games/even.py
@@ -6,7 +6,6 @@ RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def generate_round():
-    """Генерирует число и правильный ответ (yes/no)."""
     number = random.randint(1, 100)
     question = str(number)
     correct_answer = "yes" if number % 2 == 0 else "no"
